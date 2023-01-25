15710fdfdf4542ca8aade2f0e67cddf00853ea7c
Alex <alexon249@gmail.com>
2023-01-26 02:01:35 +0300

    Clear personal data

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("Doroshenko_Lab1")]
+[assembly: AssemblyTitle("WinFormsApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Doroshenko_Lab1")]
+[assembly: AssemblyCompany("github/codergrad")]
+[assembly: AssemblyProduct("WinFormsApp")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
